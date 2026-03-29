37a7ff56323ab13d04362a343d28bb88d8ce8a8c
chinasanto <acechics@gmail.com>
2026-03-30 06:54:08 +0800

    Update site favicon to use academy logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -56,17 +56,11 @@ export const metadata: Metadata = {
     icon: [
       {
         url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
+        sizes: '32x32',
+        type: 'image/png',
       },
     ],
+    shortcut: '/icon-light-32x32.png',
     apple: '/apple-icon.png',
   },
 }
